4a6fb3c5b97be420b71c0bd7b824831ea0697f05
sya-ri <sya79lua@gmail.com>
2020-11-16 15:06:32 +0900

    `brew deps --installed` の実行結果から依存関係の取得に成功

--- src/main/kotlin/me/syari/brew/non/deps/cleaner/main.kt
+++ src/main/kotlin/me/syari/brew/non/deps/cleaner/main.kt
@@ -1,5 +1,18 @@
 package me.syari.brew.non.deps.cleaner
 
 fun main() {
-    println("Hello World!")
+    val brewDepsTempFile = createTempFile().apply {
+        deleteOnExit()
+    }
+    ProcessBuilder().apply {
+        redirectOutput(brewDepsTempFile)
+        command("brew", "deps", "--installed")
+    }.start().waitFor()
+    val deps = brewDepsTempFile.readLines().associate { line ->
+        val splitLine = line.split(":")
+        val key = splitLine[0]
+        val value = splitLine[1].split("\\s+".toRegex()).filter(String::isNotEmpty)
+        key to value
+    }
+    println(deps)
 }
