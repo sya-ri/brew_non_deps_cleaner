458551321ecf940c5250b1aa87dab49571029679
sya-ri <sya79lua@gmail.com>
2020-11-16 15:16:51 +0900

    依存されていないものを出力できるようになった

--- src/main/kotlin/me/syari/brew/non/deps/cleaner/main.kt
+++ src/main/kotlin/me/syari/brew/non/deps/cleaner/main.kt
@@ -1,5 +1,9 @@
 package me.syari.brew.non.deps.cleaner
 
+data class DepTree(val deps: List<String>) {
+    var isDep = false
+}
+
 fun main() {
     val brewDepsTempFile = createTempFile().apply {
         deleteOnExit()
@@ -12,7 +16,19 @@ fun main() {
         val splitLine = line.split(":")
         val key = splitLine[0]
         val value = splitLine[1].split("\\s+".toRegex()).filter(String::isNotEmpty)
-        key to value
+        key to DepTree(value)
     }
-    println(deps)
+    var nextProcess = true
+    while (nextProcess) {
+        nextProcess = false
+        deps.values.forEach { tree ->
+            tree.deps.forEach {
+                if (deps[it]?.isDep == false){
+                    deps[it]?.isDep = true
+                    nextProcess = true
+                }
+            }
+        }
+    }
+    println(deps.entries.filter { it.value.isDep.not() }.map { it.key })
 }
